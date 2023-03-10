6b310dea9ebeb5783c713d92aebfb95be64c4ec4
Julio <despairnatural@gmail.com>
2023-03-10 17:50:23 -0300

    Lestagem dos clientes

--- src/pages/admin/cadastro/index.page.tsx
+++ src/pages/admin/cadastro/index.page.tsx
@@ -1,22 +1,38 @@
 import { RegisterCard } from "@/components/RegisterCard/RegisterCard";
 import { useLoading } from "@/hooks/useLoading/useLoading";
+import { api } from "@/lib/axios";
 
-import { Flex, Grid, Stack } from "@chakra-ui/react";
+import {
+  Card,
+  Flex,
+  Grid,
+  Stack,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
 import { NextSeo } from "next-seo";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { FaUser, FaUserMd } from "react-icons/fa";
 
-interface IStatus {
-  value: string;
-  date_last_trasnsation?: Date;
-  percentage: number;
+interface IDataClient {
+  name: string;
+  phone: string;
 }
 
+interface IResponseClient {
+  data: IDataClient[];
+}
 export default function Register() {
   const router = useRouter();
   const { showLoading, closedLoading } = useLoading();
-  const [params, setParams] = useState({
+  const [paramsClient, setParamsClient] = useState({
     perPage: 10,
     page: 1,
   });
@@ -24,15 +40,26 @@ export default function Register() {
     closedLoading();
   }, []);
 
-  //   const { data, isLoading } = useQuery<IDataTransaction>(
-  //     ["dataTableAdmin", params.page, params.perPage],
-  //     async () => {
-  //       const response = await api.get(`/admin/transaction/get-transaction`, {
-  //         params,
-  //       });
-  //       return response.data;
-  //     }
-  //   );
+  const { data: dataClient, isLoading } = useQuery<IResponseClient>(
+    ["dataTableAdmin", paramsClient.page, paramsClient.perPage],
+    async () => {
+      const params = {
+        per_page: paramsClient.perPage,
+        page: paramsClient.page,
+      };
+      const response = await api.get(`/admin/client/get-clients`, { params });
+      return response.data;
+    }
+  );
+  const columnsClients = [
+    {
+      label: "Nome",
+    },
+    {
+      label: "Telefone",
+    },
+  ];
+  const clients = dataClient?.data ? dataClient.data : [];
 
   return (
     <>
@@ -58,6 +85,65 @@ export default function Register() {
             description="Cadastro de medicos, fisioterapeltas e nutricionistas"
             icon={<FaUserMd size={"3em"} />}
           />
+        </Grid>
+        <Grid templateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)"]}>
+          <Card>
+            <Flex align="center" justify="flex-start" p="4">
+              <Text fontSize="lg" fontWeight="bold">
+                Clientes
+              </Text>
+            </Flex>
+            <Table>
+              <Thead>
+                <Tr>
+                  {columnsClients.map((column) => (
+                    <Th
+                      key={column.label}
+                      color="gray.400"
+                      borderColor={"gray.600"}
+                    >
+                      {column.label}
+                    </Th>
+                  ))}
+                </Tr>
+              </Thead>
+              <Tbody overflowY={"hidden"}>
+                {clients?.map((el, index, arr) => {
+                  return (
+                    <>
+                      <Tr key={index}>
+                        <Td
+                          fontSize="sm"
+                          fontWeight="bold"
+                          borderColor={"gray.600"}
+                          cursor="pointer"
+                          _hover={{
+                            textDecoration: "underline",
+                          }}
+                          {...(index === arr.length - 1 && {
+                            border: "none",
+                          })}
+                        >
+                          {/* <Link href={`/admin/financeiro/${el.id}`} prefetch> */}
+                          {el.name}
+                          {/* </Link> */}
+                        </Td>
+                        <Td
+                          fontSize="sm"
+                          {...(index === arr.length - 1 && {
+                            border: "none",
+                          })}
+                          borderColor={"gray.600"}
+                        >
+                          {el.phone}
+                        </Td>
+                      </Tr>
+                    </>
+                  );
+                })}
+              </Tbody>
+            </Table>
+          </Card>
         </Grid>
       </Stack>
     </>
---
+++ src/pages/api/admin/client/get-clients.api.ts
@@ -0,0 +1,52 @@
+import { prisma } from "@/lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+import { z } from "zod";
+import { buildNextAuthOption } from "../../auth/[...nextauth].api";
+
+const createClientSchema = z.object({
+  name: z.string(),
+  observation: z.string(),
+  cpf: z.string(),
+  date_of_birth: z.string(),
+  phone: z.string(),
+});
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  if (request.method !== "GET") {
+    return response.status(405).end();
+  }
+
+  const session = await getServerSession(
+    request,
+    response,
+    buildNextAuthOption(request, response)
+  );
+
+  // if (!session) {
+  //   return response.status(401).end();
+  // }
+  const { per_page, page } = request.query;
+
+  const current_page = page ? Number(page) : 1;
+  const take = per_page ? Number(per_page) : 10; // Número de registros por página
+  const skip = (current_page - 1) * take; // Número de registros a serem pulados
+  const total = await prisma.clients.count();
+  const total_pages = Math.ceil(total / take);
+  const transation = await prisma.clients.findMany({
+    take,
+    skip,
+    orderBy: {
+      created_at: "desc",
+    },
+  });
+
+  return response
+    .status(200)
+    .json({ data: transation, per_page, total, page, total_pages });
+
+  return response.status(201).end();
+}
